21d0aa9dcd614e40ac6e8163f754cbdafccae47c
dinamio <dinamio@ukr.net>
2015-01-11 22:57:28 +0200

    [ISSUE-1] Commit mainPhoto(e.godun)

---
+++ src/main/java/com/sloboda/site/entity/MainPhoto.java
@@ -0,0 +1,25 @@
+package com.sloboda.site.entity;
+
+public class MainPhoto {
+	
+	Photo photo;
+	
+	String description;
+
+	public Photo getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(Photo photo) {
+		this.photo = photo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
